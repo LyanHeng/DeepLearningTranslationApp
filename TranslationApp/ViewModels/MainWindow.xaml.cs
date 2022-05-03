422afc4991f47960dec95faaae2a34da8217a2d3
LyanAzidie <lyan.heng18@gmail.com>
2022-05-03 18:33:34 +1000

    Integrate Language Detection & Error Messages

--- TranslationApp/ViewModels/MainWindow.xaml.cs
+++ TranslationApp/ViewModels/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Windows;
+using System.Collections.Generic;
 using Google.Cloud.Translation.V2;
 using Microsoft.Win32;
 
@@ -8,26 +9,80 @@ namespace TranslationApp
 {
     public partial class MainWindow : Window
     {
+        private Dictionary<string, string> m_languagesKeys = new Dictionary<string, string>();
+        private TranslationClient m_client = TranslationClient.CreateFromApiKey(Environment.GetEnvironmentVariable("api_key"));
+
+        public Dictionary<string, string> LanguageKeys { get => m_languagesKeys; set => m_languagesKeys = value; }
+        public TranslationClient Client { get => m_client; }
+
         public MainWindow()
         {
             InitializeComponent();
+            PopulateLanguageComboBoxes();
         }
 
+        // retrieve all supported languages from Google Service
+        private void PopulateLanguageComboBoxes()
+        {
+            // add default auto-detection
+            box1.Items.Add("Auto");
+
+            // get all supported language by Google
+            // "en" - defines the language of all the names of the languages
+            IList<Language> supportedLanguages = Client.ListLanguages("en");
+            foreach (Language language in supportedLanguages)
+            {
+                if (!LanguageKeys.ContainsKey(language.Name))
+                {
+                    LanguageKeys.Add(language.Name, language.Code);
+                    box1.Items.Add(language.Name);
+                    box2.Items.Add(language.Name);
+                }
+            }
+        }
+
+        // API call using Google Cloud Library
         private void Translate(object sender, RoutedEventArgs e)
         {
-            if (textToTranslate.Text.Length >= 5000)
+            // clear past messages
+            errorMessage.Text = "";
+            translatedText.Text = "";
+            errorMessage.Visibility = Visibility.Hidden;
+            // check text length
+            string text = textToTranslate.Text;
+            if (text.Length >= 5000)
             {
-                translatedText.Text = "Google API does not support translation above 5000 characters.";
+                errorMessage.Text = "Google API does not support translation above 5000 characters.";
                 return;
             }
-            
             // retrieve API Key from Environment variable set up
-            // TODO v0.5 - set up the key in a config file
-            var client = TranslationClient.CreateFromApiKey(Environment.GetEnvironmentVariable("api_key"));
-            var response = client.TranslateText(textToTranslate.Text, LanguageCodes.French);
-
-            translatedText.Text = response.TranslatedText;
+            Detection languageDetected = box1.SelectedItem.ToString() != "Auto" ? Client.DetectLanguage(text) : null;
+            // translate
+            try
+            {
+                // check language is the same as requested language
+                if (languageDetected == null)
+                {
+                    translatedText.Text = Client.TranslateText(text, LanguageKeys[box2.SelectedItem.ToString()]).TranslatedText;
+                }
+                else if (languageDetected.Language == LanguageKeys[box1.SelectedItem.ToString()])
+                {
+                    translatedText.Text = Client.TranslateText(text, LanguageKeys[box2.SelectedItem.ToString()], LanguageKeys[box1.SelectedItem.ToString()]).TranslatedText;
+                }
+                else
+                {
+                    errorMessage.Visibility = Visibility.Visible;
+                    errorMessage.Text = "Language Defined Did not Match";
+                }
+            }
+            catch (Exception exc)
+            {
+                errorMessage.Visibility = Visibility.Visible;
+                errorMessage.Text = "Unexpected Error: " + exc.ToString();
+            }
         }
+
+        #region Handlers
         private void btnOpenFile_Click(object sender, RoutedEventArgs e)
         {
             // Create OpenFileDialog
@@ -52,5 +107,6 @@ namespace TranslationApp
             //and to save the settings
             Properties.Settings.Default.Save();
         }
+        #endregion
     }
 }
