d2e050b0e6e87aa4d92295b6fb920ef93bab07bb
Jamie <jamiekozminska@gmail.com>
2022-05-19 21:00:14 +1000

    Removed navigation pane at the top between pages. Aligned some elements

--- TranslationApp/ViewModels/SingleLanguagePage.xaml.cs
+++ TranslationApp/ViewModels/SingleLanguagePage.xaml.cs
@@ -20,7 +20,7 @@ namespace TranslationApp
         public SingleLanguagePage()
         {
             InitializeComponent();
-            PopulateLanguageComboBoxes();
+            PopulateLanguageComboBoxes(); 
         }
 
         #region Methods
